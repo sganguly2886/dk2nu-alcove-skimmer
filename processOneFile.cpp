daad9926991d3d90af59fc2dd38271d8d0a7cd88
barlase <48187620+barlase@users.noreply.github.com>
2025-07-09 12:14:37 -0500

    Update processOneFile.cpp

--- processOneFile.cpp
+++ processOneFile.cpp
@@ -1,3 +1,4 @@
+#include "dk2nuTree.h"
 #include "alcoveTree.h"
 #include <TFile.h>
 #include <TTree.h>
@@ -12,39 +13,54 @@ int main(int argc, char* argv[]) {
 
   TString inputFile = argv[1];
 
-  // Try to open the input ROOT file
-  TFile* file = new TFile(inputFile, "READ");
-  if (!file || file->IsZombie()) {
+  // Open input ROOT file
+  TFile* input = new TFile(inputFile, "READ");
+  if (!input || input->IsZombie()) {
     std::cerr << "Error: Cannot open file " << inputFile << std::endl;
     return 1;
   }
 
-  // Load AlcoveTracks tree
+  // Get trees
+  TTree* dk2nuTreePtr = nullptr;
   TTree* alcoveTreePtr = nullptr;
-  file->GetObject("AlcoveTracks", alcoveTreePtr);
+  input->GetObject("dk2nuTree", dk2nuTreePtr);
+  input->GetObject("AlcoveTracks", alcoveTreePtr);
 
-  if (!alcoveTreePtr) {
-    std::cerr << "Error: AlcoveTracks tree not found in " << inputFile << std::endl;
-    file->Close();
-    delete file;
+  if (!dk2nuTreePtr || !alcoveTreePtr) {
+    std::cerr << "Error: Missing dk2nuTree or AlcoveTracks in " << inputFile << std::endl;
+    input->Close();
+    delete input;
     return 1;
   }
 
-  // Generate a skim tag based on the input filename
+  // Generate tag from input filename
   std::string fname = inputFile.Data();
   size_t lastSlash = fname.find_last_of('/');
-  std::string tag = fname.substr(lastSlash + 1);  // e.g. neutrino_09913.dk2nu.root
-  tag = tag.substr(0, tag.find(".dk2nu.root"));   // strip .dk2nu.root
+  std::string tag = fname.substr(lastSlash + 1);
+  tag = tag.substr(0, tag.find(".dk2nu.root"));
 
-  // Prepare and run the skimmer
-  alcoveTree myTree;
-  myTree._fnameid = tag;
-  myTree.ofname = tag + ".skim.root";
-  myTree.SetAlcoveTree(alcoveTreePtr);
-  myTree.Loop();
+  // Create output file for both skims
+  TFile* output = new TFile((tag + ".combined.root").c_str(), "RECREATE");
 
-  file->Close();
-  delete file;
+  // Run DK2NU skim
+  dk2nuTree dkSkimmer(dk2nuTreePtr);
+  dkSkimmer._fnameid = tag;
+  dkSkimmer.Loop(output);
+
+  // Run Alcove skim
+  alcoveTree alcSkimmer;
+  alcSkimmer._fnameid = tag;
+  alcoveTreePtr->SetMakeClass(1);  // Ensure compatibility if needed
+  alcSkimmer.SetAlcoveTree(alcoveTreePtr);
+  alcSkimmer.Loop(output);
+
+  // Clean up
+  output->Close();
+  delete output;
+  input->Close();
+  delete input;
+
+  std::cout << "Combined skim written to: " << tag << ".combined.root" << std::endl;
 
   return 0;
 }
